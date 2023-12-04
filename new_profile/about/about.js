62aa6afd92fe17a2e6aa954bfa533d823d56c628
Gemmy924 <148513597+Gemmy924@users.noreply.github.com>
2023-12-04 11:33:54 +0900

    Add files via upload

---
+++ new_profile/about/about.js
@@ -0,0 +1,10 @@
+// ******* hamberger menu *********
+//ボタンをクリックした際のアニメーションの設定
+$(".openbtn").click(function () {//openbtnがクリックされたら
+  $(this).toggleClass('active');//openbtn自身に activeクラスを付与し
+  $("#nav_target").toggleClass('panelactive');//#naviにpanelactiveクラスを付与
+});
+$("#nav_target li a").click(function () {//ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+  $("#nav_target").removeClass('panelactive');//ヘッダーのpanelactiveクラスも除去
+});
